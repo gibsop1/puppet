789a8ba25ef2b9e278d68caec3a87bf0a64768f7
root <root@precise32.(none)>
2014-04-27 12:06:34 +0000

    Made +x

--- puppet-setup.sh
+++ puppet-setup.sh
@@ -1,9 +1,8 @@
 #!/bin/env bash
 
-puppet module install jfryman-nginx
-puppet module install puppetlabs-vcsrepo
 wget https://apt.puppetlabs.com/puppetlabs-release-precise.deb
 sudo dpkg -i puppetlabs-release-precise.deb
 sudo apt-get update
 sudo apt-get install puppet-common
+puppet module install puppetlabs-vcsrepo
 puppet module install jfryman-nginx
